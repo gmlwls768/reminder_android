b1feaa7cb1cec1f574b7eaf3d47569df95af6a7f
minnnisu <mine52100@naver.com>
2022-11-17 01:58:27 +0900

    목록 직접 추가기능 구현 및 디자인 개선

--- app/src/main/java/com/example/reminder_project/CompleteActivity.java
+++ app/src/main/java/com/example/reminder_project/CompleteActivity.java
@@ -1,19 +1,86 @@
 package com.example.reminder_project;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CompleteActivity extends AppCompatActivity {
+    public static final int FIELD_NAME_ID = 0;
+    public static final int FIELD_NAME_TITLE = 1;
+    public static final int FIELD_NAME_CONTENTS = 2;
+    public static final int FIELD_NAME_PRIORITY = 3;
+    public static final int FIELD_NAME_LOCATION = 4;
+    public static final int FIELD_NAME_ON_CREATE = 5;
+    public static final int FIELD_NAME_IS_COMPLETE = 6;
+    public static final int FIELD_NAME_ID_ON_COMPLETED = 7;
+
+    ToDoTable todo; //A helper class to manage database creation and version management.
+    SQLiteDatabase sqlDB;
+    Cursor cursor;
+    Intent intent;
+    LinearLayout listLayoutParent;
     Button todoBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
+        todo = new ToDoTable(this);
+        sqlDB = todo.getWritableDatabase(); //Create and/or open a database
+        cursor = sqlDB.rawQuery("SELECT * FROM toDo WHERE IS_COMPLETE = 1;", null); //데이터베이스의 모든 값을 가져옴
         todoBtn = findViewById(R.id.todoBtn);
+        listLayoutParent = (LinearLayout) findViewById(R.id.list_layout_parent);
+
+        while (cursor.moveToNext()) {
+            LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT); //너비, 높이 설정 단축어
+            LinearLayout.LayoutParams w = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.MATCH_PARENT); //너비, 높이 설정 단축어
+            LinearLayout listItemBox= new LinearLayout(getApplicationContext()); // 각각의 할일 목록을 감싸는 레이아웃
+            listItemBox.setLayoutParams(p); //너비, 높이 설정
+            listItemBox.setOrientation(LinearLayout.HORIZONTAL); // 수평 정렬
+            int listId = Integer.parseInt(cursor.getString(FIELD_NAME_ID));
+            CheckBox cb = new CheckBox(getApplicationContext()); // listItemBox 내 사용될 CheckBox
+            cb.setLayoutParams(w);
+
+            TextView tv= new TextView(getApplicationContext()); // listItemBox 내 사용될 TextView
+            tv.setLayoutParams(w);
+            tv.setText(cursor.getString(FIELD_NAME_TITLE)); // View의 텍스트 값을 toDoTable의 TITLE 컬럼의 값으로 설정
+
+            TextView dbIdView= new TextView(getApplicationContext()); //listItemBox 내 사용될 TextView
+            dbIdView.setLayoutParams(w);
+            dbIdView.setVisibility(View.GONE);
+            dbIdView.setText(cursor.getString(FIELD_NAME_ID)); // View의 텍스트 값을 toDoTable의 ID 컬럼의 값으로 설정
+
+            cb.setOnClickListener(new View.OnClickListener() { // 클릭 시 해당 목록이 완료된 항목으로 이동
+                @Override
+                public void onClick(View view) {
+                    sqlDB.execSQL("DELETE FROM toDo WHERE ID = " + listId + ";");
+                    finish();//인텐트 종료
+                    overridePendingTransition(0, 0);//인텐트 효과 없애기
+                    intent = getIntent(); //인텐트
+                    startActivity(intent); //액티비티 열기
+                    overridePendingTransition(0, 0);//인텐트 효과 없애기
+                }
+            });
+
+            listItemBox.addView(cb); //자식 위젯 설절
+            listItemBox.addView(tv); //자식 위젯 설절
+            listLayoutParent.addView(listItemBox); //자식 위젯 설절
+        }
+        cursor.close();
+
         todoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/example/reminder_project/WorkActivity.java
+++ app/src/main/java/com/example/reminder_project/WorkActivity.java
@@ -1,8 +1,11 @@
 package com.example.reminder_project;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -12,12 +15,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+
+@RequiresApi(api = Build.VERSION_CODES.O)
 public class WorkActivity extends AppCompatActivity {
     //Database Filed
     public static final int FIELD_NAME_ID = 0;
@@ -30,80 +41,169 @@ public class WorkActivity extends AppCompatActivity {
     public static final int FIELD_NAME_ID_ON_COMPLETED = 7;
 
 
-
+    int y = 0, m = 0, d = 0, h = 0, mi = 0; // 사용자가 설정한 시간
     ToDoTable todo; //A helper class to manage database creation and version management.
     SQLiteDatabase sqlDB;
     Cursor cursor;
+    RadioGroup priorityGroup;
     Button saveBtn;
-    Switch timeSetSwitch;
-    TimePicker timeSetTimepicker;
-    DatePicker timeSetDatepicker;
-    RadioButton highPriority,mediumPriority,lowPriority,nonePriority;
+    ImageButton timeBtn, chkBtn;
+    RadioButton highPriority, mediumPriority, lowPriority, nonePriority;
     EditText placeInfo, title, content;
+    TextView timeText;
+    LocalDateTime now= LocalDateTime.now();
+    String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy/MM/dd/HH/mm"));
+    String timeNow [] = formatedNow.split("/");
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar);
         todo = new ToDoTable(this);
         title = (EditText) findViewById(R.id.title);
         content = (EditText) findViewById(R.id.content);
+        priorityGroup = (RadioGroup) findViewById(R.id.priorityGroup);
         highPriority = (RadioButton) findViewById(R.id.highPriority);
         mediumPriority = (RadioButton) findViewById(R.id.mediumPriority);
         lowPriority = (RadioButton) findViewById(R.id.lowPriority);
         nonePriority = (RadioButton) findViewById(R.id.nonePriority);
         saveBtn = (Button) findViewById(R.id.saveBtn);
-        timeSetSwitch = (Switch) findViewById(R.id.timeSetSwitch);
-        timeSetTimepicker = (TimePicker) findViewById(R.id.timeSetTimepicker);
-        timeSetDatepicker = (DatePicker) findViewById(R.id.timeSetDatepicker);
         placeInfo = (EditText) findViewById(R.id.placeInfo);
-
+        timeBtn = (ImageButton) findViewById(R.id.timeBtn);
+        chkBtn = (ImageButton) findViewById(R.id.chkBtn);
+        timeText = (TextView) findViewById(R.id.timeText);
 
         Intent intent = getIntent(); //액티비티간에 인수와 리턴값을 전달
         int id = intent.getIntExtra("id", -1); //toDoList의 id 컬럼 값
-        if(id != -1){ //id 컬럼 값이 있을 경우(수정 페이지)
+        if (id != -1) { //id 컬럼 값이 있을 경우(수정 페이지)
             sqlDB = todo.getReadableDatabase(); //Create and/or open a database
             cursor = sqlDB.rawQuery("SELECT * FROM toDo WHERE ID =" + Integer.toString(id) + ";", null); //id에 해당하는 컬럼의 값을 가져옴
             cursor.moveToFirst(); //커서를 첫번째로 이동
-//            if(cursor.getString(FIELD_NAME_ON_CREATE) != null){
-//
-//            }
+            int userPriority = Integer.parseInt(cursor.getString(FIELD_NAME_PRIORITY));
+            switch (userPriority){ //우선순위 설정
+                case 0:
+                    priorityGroup.check(R.id.nonePriority);
+                    break;
+                case 1:
+                    priorityGroup.check(R.id.lowPriority);
+                    break;
+                case 2:
+                    priorityGroup.check(R.id.mediumPriority);
+                    break;
+                case 3:
+                    priorityGroup.check(R.id.highPriority);
+                    break;
+            }
+
+            title.setText(cursor.getString(FIELD_NAME_TITLE)); //타이틀 설정
+
+            if(cursor.getString(FIELD_NAME_CONTENTS) != null){ //할일 내용 설절
+                content.setText(cursor.getString(FIELD_NAME_CONTENTS));
+            }
+
+            if(cursor.getString(FIELD_NAME_ON_CREATE) != null){ //알림시간 설정
+                String userAlertTime [] = cursor.getString(FIELD_NAME_ON_CREATE).split("/");
+                y = Integer.parseInt(userAlertTime[0]);
+                m = Integer.parseInt(userAlertTime[1]);
+                d = Integer.parseInt(userAlertTime[2]);
+                h = Integer.parseInt(userAlertTime[3]);
+                mi = Integer.parseInt(userAlertTime[4]);
+                timeText.setText(y + "." + m + "." + d + "\n" + h + ":" + mi);
+            }
+
+            if(cursor.getString(FIELD_NAME_LOCATION) != null){ //장소 설정
+                placeInfo.setText(cursor.getString(FIELD_NAME_LOCATION));
+            }
 
         }
 
 
-        timeSetSwitch.setOnClickListener(new View.OnClickListener() {
+        timeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (timeSetSwitch.isChecked()){
-                timeSetTimepicker.setVisibility(View.VISIBLE);
-                timeSetDatepicker.setVisibility(View.VISIBLE);
-                }
-                else{
-                    timeSetTimepicker.setVisibility(View.GONE);
-                    timeSetDatepicker.setVisibility(View.GONE);
-                }
+                showTime();
+                showDate();
             }
         });
-        timeSetDatepicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
-            @Override
-            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
 
-            }
-        });
-        timeSetTimepicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
-            @Override
-            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
 
+        chkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                timeText.setText("Date: " +y + "." + m + "." + d + "\n" + "Time: " + h + ":" + mi);
             }
         });
+
+
+
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String myTitle = title.getText().toString();
+                String myContents = content.getText().toString();
+                String myPriority = "0";
+                switch (priorityGroup.getCheckedRadioButtonId()){
+                    case R.id.nonePriority:
+                        myPriority = "0";
+                        break;
+                    case R.id.lowPriority:
+                        myPriority = "1";
+                        break;
+                    case R.id.mediumPriority:
+                        myPriority = "2";
+                        break;
+                    case R.id.highPriority:
+                        myPriority = "3";
+                        break;
+                }
+                String myLocation = placeInfo.getText().toString();
+                sqlDB = todo.getWritableDatabase();
+                if(y != 0){
+                    String myAlert = Integer.toString(y) + "/" + Integer.toString(m) + "/" + Integer.toString(d) + "/" + Integer.toString(h) + "/" + Integer.toString(mi);
+                    sqlDB.execSQL("INSERT INTO toDo (TITLE, CONTENTS, PRIORITY, LOCATION, ALERT , ON_CREATE) VALUES('" + myTitle + "','" + myContents + "', '" + myPriority + "', '" + myLocation + "', '" + myAlert + "', '" + formatedNow + "');"); //toDoTable 데이터 추가
+                }else {
+                    sqlDB.execSQL("INSERT INTO toDo (TITLE, CONTENTS, PRIORITY, LOCATION, ON_CREATE) VALUES('" + myTitle + "','" + myContents + "', '" + myPriority + "', '" + myLocation + "', '" + formatedNow + "');"); //toDoTable 데이터 추가
+                }
+                sqlDB.close();
+                Toast.makeText(getApplicationContext(), "할일목록을 저장하였습니다!", Toast.LENGTH_SHORT).show();
                 finish();
-                Toast.makeText(getApplicationContext(),"할일목록을 저장하였습니다!",Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
+    }
+
+
+    void showDate() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                y = year;
+                m = month + 1;
+                d = dayOfMonth;
+
+            }
+        },Integer.parseInt(timeNow[0]),Integer.parseInt(timeNow[1]),Integer.parseInt(timeNow[2]));
+        datePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        datePickerDialog.setMessage("날짜를 선택하시오");
+        datePickerDialog.show();
+    }
+
+    void showTime() {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,android.R.style.Theme_Holo_Light_Dialog_NoActionBar, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                h = hourOfDay;
+                mi = minute;
+
+            }
+        }, Integer.parseInt(timeNow[3]), Integer.parseInt(timeNow[4]), true);
+        timePickerDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        timePickerDialog.setMessage("시간을 선택하시오");
+        timePickerDialog.show();
+//        timePickerDialog.onTimeChanged(timePickerDialog.setContentView(this););
     }
 }
